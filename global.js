4fb7489b3b07f0cd67e9e0fc34609e29ce37719b
Johny Nguyen <jon009@ucsd.edu>
2026-04-17 21:38:44 -0700

    added defer

--- global.js
+++ global.js
@@ -30,6 +30,7 @@ for (let p of pages) {
   let url = p.url;
   let title = p.title;
   // next step: create link and add it to nav
+  
   const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
   ? "/"                  // Local server
   : "/website/";         // GitHub Pages repo name
