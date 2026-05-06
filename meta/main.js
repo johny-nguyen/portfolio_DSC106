37420e6e855d2fbb9eededf9a28eef6271df3ea3
Johny Nguyen <jon009@ucsd.edu>
2026-05-06 02:07:54 -0700

    Completed Step 2 of Lab 5

--- meta/main.js
+++ meta/main.js
@@ -13,4 +13,158 @@ async function loadData() {
   return data;
 }
 
+
+function processCommits(data) {
+  return d3
+    .groups(data, (d) => d.commit)
+    .map(([commit, lines]) => {
+      let first = lines[0];
+      let { author, date, time, timezone, datetime } = first;
+      let ret = {
+        id: commit,
+        url: 'https://github.com/portfolio_DSC106/commit/' + commit,
+        author,
+        date,
+        time,
+        timezone,
+        datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+      };
+
+      Object.defineProperty(ret, 'lines', {
+        value: lines,
+        // What other options do we need to set?
+        // Hint: look up configurable, writable, and enumerable
+        writeable: false,
+        enumerable: false,
+        configurable: false
+      });
+
+      return ret;
+    });
+}
+
+function renderCommitInfo(data, commits) {
+  // Create the dl element
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+
+  // Add total LOC
+  dl.append('dt').html('TOTAL <abbr title="Lines of code">LOC</abbr>');
+  dl.append('dd').text(data.length);
+
+  // Add total commits
+  dl.append('dt').text('TOTAL COMMITS');
+  dl.append('dd').text(commits.length);
+
+  // Add more stats as needed...
+  // Add Files
+  dl.append('dt').text('TOTAL FILES');
+  dl.append('dd').text(d3.group(data, (d) => d.file).size);
+
+  //Time of Day
+    const workByPeriod = d3.rollups(
+    data,
+    (v) => v.length,
+    (d) => new Date(d.datetime).toLocaleString('en', { dayPeriod: 'short' }),
+    );
+
+    const maxPeriod = d3.greatest(workByPeriod, (d) => d[1])?.[0];
+
+  dl.append('dt').text('WHEN WORK IS DONE');
+  dl.append('dd').text(maxPeriod);
+
+  // Average File Length
+  const fileLengths = d3.rollups(
+  data,
+  (v) => d3.max(v, (v) => v.line),
+  (d) => d.file,
+ );
+  dl.append('dt').text('AVERAGE FILE LENGTH');
+  dl.append('dd').text(d3.mean(fileLengths, (d) => d[1]));
+
+}
+
+function renderScatterPlot(data, commits) {
+  // Put all the JS code of Steps inside this function
+  const width = 1000;
+  const height = 600;
+  const svg = d3
+  .select('#chart')
+  .append('svg')
+  .attr('viewBox', `0 0 ${width} ${height}`)
+  .style('overflow', 'visible');
+
+
+  const xScale = d3
+  .scaleTime()
+  .domain(d3.extent(commits, (d) => d.datetime))
+  .range([0, width])
+  .nice();
+
+  const yScale = d3.scaleLinear().domain([24, 0]).range([height, 0]);
+  
+  const dots = svg.append('g').attr('class', 'dots');
+
+  dots
+  .selectAll('circle')
+  .data(commits)
+  .join('circle')
+  .attr('cx', (d) => xScale(d.datetime))
+  .attr('cy', (d) => yScale(d.hourFrac))
+  .attr('r', 5)
+  .attr('fill', 'steelblue');
+
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+  const usableArea = {
+  top: margin.top,
+  right: width - margin.right,
+  bottom: height - margin.bottom,
+  left: margin.left,
+  width: width - margin.left - margin.right,
+  height: height - margin.top - margin.bottom,
+  };
+
+// Update scales with new ranges
+  xScale.range([usableArea.left, usableArea.right]);
+  yScale.range([usableArea.bottom, usableArea.top]);
+
+  // Create the axes
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3
+  .axisLeft(yScale)
+  .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+// Add gridlines BEFORE the axes
+const gridlines = svg
+  .append('g')
+  .attr('class', 'gridlines')
+  .attr('transform', `translate(${usableArea.left}, 0)`);
+    
+// Create gridlines as an axis with no labels and full-width ticks
+gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+  
+// Add X axis
+svg
+  .append('g')
+  .attr('transform', `translate(0, ${usableArea.bottom})`)
+  .call(xAxis);
+
+// Add Y axis
+svg
+  .append('g')
+  .attr('transform', `translate(${usableArea.left}, 0)`)
+  .call(yAxis);
+
+
+}
+
+
 let data = await loadData();
+
+let commits = processCommits(data);
+
+renderCommitInfo(data, commits);
+renderScatterPlot(data, commits);
+
